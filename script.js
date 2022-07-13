2dcaebf062d8e1ba321113e542740d4449ece6b4
Erika Brownlee <eebrownl@gmail.com>
2022-07-13 12:34:47 -0600

    Text now prints to screen. Need to style

--- script.js
+++ script.js
@@ -20,7 +20,40 @@ const addBook = (e) => {
     document.querySelector('form').reset();
     console.log(newBook);
     console.log(myLibrary);
+    addBox()
+
 }
 
 document.getElementById('btn').addEventListener('click', addBook)
 
+function addBox() {
+
+    let outputBox = document.createElement('div');
+    outputBox.classList.add('output-box');
+    let bookInfo = document.createElement('p');
+    let titleInfo = document.createElement('span')
+    let authorInfo = document.createElement('span')
+    let pagesInfo = document.createElement('span')
+    titleInfo.classList.add('info-styling');
+    authorInfo.classList.add('info-styling');
+    pagesInfo.classList.add('info-styling');
+    bookInfo.append(titleInfo, pagesInfo, authorInfo);
+    bookInfo.classList.add('book-info')
+    document.querySelector('.right-container').appendChild(outputBox);
+    outputBox.appendChild(bookInfo);
+
+    myLibrary.forEach((book) => {
+        titleInfo.textContent = `${book.title}`;
+        authorInfo.textContent = `${book.author}`;
+        pagesInfo.textContent = `${book.pages}`;
+    })
+    
+    
+    // function createBoxText() {
+    //     for (let i = 0; i < myLibrary.length; i++) {
+    //     let obj = myLibrary[i]; 
+    //     bookInfo.textContent = obj.title;
+    }
+
+//     createBoxText();
+// }
